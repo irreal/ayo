bce0d65243fea807b72cea5adcd21ae5b3ef8a00
Milos Spasojevic <milos.s.pfc@gmail.com>
2022-07-22 13:18:16 +0200

    lave, ustani, zapni, kreni... cao!

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,21 @@ import Head from 'next/head'
 import useSound from 'use-sound';
 
 const Home: NextPage = () => {
-  const [play] = useSound('ayo.mp3');
+  const [playAyo] = useSound('ayo.mp3');
+  const [playLave] = useSound('lave.mp3');
+  const [playKreni] = useSound('kreni.mp3');
+  const [playCao] = useSound('cao.mp3');
   const ayoClick =()=> {
-    play();
+    playAyo();
+  }
+  const laveClick =()=> {
+    playLave();
+  }
+  const kreniClick =()=> {
+    playKreni();
+  }
+  const caoClick =()=> {
+    playCao();
   }
   return (
     <div className='h-full'>
@@ -15,8 +27,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='h-full flex flex-col justify-center items-center'>
-      <button onClick={ayoClick} id="launch" className='bg-red-500 rounded-full w-52 h-52 text-white text-4xl uppercase font-extrabold text-center'><span>AYOOO</span>
+      <main className='h-full flex flex-col gap-24  justify-center items-center'>
+      <button onClick={ayoClick} className='button-shadow bg-red-500 rounded-full w-52 h-52 text-white text-4xl uppercase font-extrabold text-center'><span>AYOOO</span>
+  </button>
+      <button onClick={laveClick} className='button-shadow bg-red-500 rounded-full w-52 h-52 text-white text-4xl uppercase font-extrabold text-center'><span>LAVE</span>
+  </button>
+      <button onClick={kreniClick} className='button-shadow bg-red-500 rounded-full w-52 h-52 text-white text-4xl uppercase font-extrabold text-center'><span>KRENI</span>
+  </button>
+      <button onClick={caoClick} className='button-shadow bg-red-500 rounded-full w-52 h-52 text-white text-4xl uppercase font-extrabold text-center'><span>ĆAO!</span>
   </button>
       </main>
     </div>
